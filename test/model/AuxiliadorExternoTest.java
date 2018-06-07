791c63b371a66d75abafe0d3599c0ad32754487b
Elimar Macena <elimar.macena@gmail.com>
2018-06-07 08:42:07 -0300

    melhoria organizacao

---
+++ test/model/AuxiliadorExternoTest.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import model.AuxiliadorExterno;
+class TestAuxiliador {
+	
+@BeforeClass
+static void setUpBeforeClass() throws Exception {
+
+}
+
+        
+@AfterClass
+static void tearDownAfterClass() throws Exception {
+}
+
+@BeforeClass
+void setUp() throws Exception {
+}
+
+@After
+void tearDown() throws Exception {
+}
+
+@Test
+void testEnvioInfoAcidente() {
+    //informacoes para escrita
+    String auxEsperado = "joaozin";
+    AuxiliadorExterno tAuxiliador = new AuxiliadorExterno();
+    tAuxiliador.setNome(auxEsperado);
+    tAuxiliador.setOrgaoAssociado("samu");
+    tAuxiliador.envioInfoAcidente("PPX-4111", 2, "333442", new Date());		
+    //confirmacao da escrita no arquivo
+    BufferedReader bufferLeitura = null;
+    String linha = "";
+    String[] vetLinha= null;
+    try {
+        bufferLeitura = new BufferedReader(new FileReader(".\\acidentes_info.csv") );
+        linha = bufferLeitura.readLine();
+        vetLinha = linha.split(";");
+        assertEquals(auxEsperado,vetLinha[0]);
+    }
+    catch(IOException rd) {
+        rd.printStackTrace();
+    }
+    finally {
+        try {
+            bufferLeitura.close();
+        }
+        catch(IOException c) {
+            c.printStackTrace();
+        }
+    }
+}
+}
